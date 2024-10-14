ed771ea8cfa2e784f3914c0c9a3f1290ae916dac
Tony Mwangi Mugi <tonymugi074@gmail.com>
2024-10-14 20:40:26 +0300

    Update App.jsx

--- personal-expense-tracker/src/App.jsx
+++ personal-expense-tracker/src/App.jsx
@@ -1,12 +1,13 @@
 import { Route, Routes, Navigate } from 'react-router-dom';
 import React, { useState, useEffect } from 'react';
 import Dashboard from './Page/Dashboard';
-
+import Transactions from './Page/Transactions';
 function App() {
   return (
     <div className="App">
       <Routes>
             <Route path="/" element={<Dashboard />} />
+            <Route path="/transactions" element={<Transactions />} />
       </Routes>
     </div>
   );
