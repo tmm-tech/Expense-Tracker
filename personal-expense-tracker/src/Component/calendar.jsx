1a065a3fe1ec80cc214839223ab42d59ae1ee9a9
Tony Mwangi Mugi <tonymugi074@gmail.com>
2024-10-21 20:03:50 +0300

    Update calendar.jsx

--- personal-expense-tracker/src/Component/calendar.jsx
+++ personal-expense-tracker/src/Component/calendar.jsx
@@ -11,6 +11,7 @@ function Calendar({
   ...props
 }) {
   return (
+      console.log(buttonVariants({ variant: "outline" }));
     (<DayPicker
       showOutsideDays={showOutsideDays}
       className={cn("p-3", className)}
@@ -21,7 +22,7 @@ function Calendar({
         caption_label: "text-sm font-medium",
         nav: "space-x-1 flex items-center",
         nav_button: cn(
-          console.log(buttonVariants({ variant: "outline" }));
+        
           buttonVariants({ variant: "outline" }),
           "h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100"
         ),
