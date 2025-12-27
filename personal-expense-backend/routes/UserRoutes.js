761ef2030cbbb5158636cdc942b6817b62616564
tmm-tech <tonymugi074@gmail.com>
2025-12-27 22:48:57 +0300

    Aurex Backend

--- personal-expense-backend/routes/UserRoutes.js
+++ personal-expense-backend/routes/UserRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const   UserRoutes = express.Router();
 const { createSession } = require('../controller/UserControllers');
-import { authMiddleware } from '../middleware/auth';
+import { requireAuth } from '../middleware/auth';
 
 // Create and insert a new user session
-UserRoutes.post('/sync', createSession);
+UserRoutes.post('/sync', requireAuth, createSession);
 
 module.exports = UserRoutes;
